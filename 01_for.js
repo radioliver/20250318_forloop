5374e78b1eebd22c9a4f694b29a539f43aa8b005
radioliver <radi.oliver@diak.szbi-pg.hu>
2025-03-18 10:21:58 +0100

    loops

---
+++ 01_for.js
@@ -0,0 +1,38 @@
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+
+
+// let person = {name:"Pista", age:40, email:"pista@email.com"};
+// for(key in person) {
+//     console.log(`${key} = ${person[key]}`);
+// } 
+
+
+// let i = 1;
+// while (i <= 5) {
+//     console.log(i);
+//     i++
+// }
+
+
+// let i2 = 1;
+// do {
+//     console.log(i);
+//     i2++;
+// } while (i <= 5);
+
+
+// let i3 = 1;
+// while (true) {
+// if (i3 > 5) {
+// break;
+// }
+// console.log(i);
+// i3++;
+// }
+
+
+
+
+
